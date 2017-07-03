ebb00ca6f677cec476595e2612ac524522e762bb
Beta_Cao <beta_cao@Beta-Caos-MacBook-Pro.local>
2017-07-03 17:08:27 +0800

    add new feature

---
+++ HMWKWebViewHandler/Classes/HMWKWebViewHandler.m
@@ -0,0 +1,13 @@
+//
+//  HMWKWebViewHandler.m
+//  HMWKWebViewHandler
+//
+//  Created by Beta_Cao on 03/07/2017.
+//  Copyright © 2017 Beta_Cao. All rights reserved.
+//
+
+#import "HMWKWebViewHandler.h"
+
+@implementation HMWKWebViewHandler
+
+@end
---
+++ HMWKWebViewHandler/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HMWKWebViewHandler
+//
+//  Created by Beta_Cao on 03/07/2017.
+//  Copyright © 2017 Beta_Cao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
